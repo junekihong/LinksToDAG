b1ec38418d9972f70cf7f9ab53ff2f2d6dc59e4b
Juneki Hong <junekihong@gmail.com>
2014-05-03 05:20:10 -0400

    modified trials.sh to plot the precision recall results as well

--- trials.sh
+++ trials.sh
@@ -10,17 +10,17 @@ END=100
 
 for ((i=1;i<=$END;i++));
 do
-    bash src/tools/trial.sh data/english_bnews_train.sentences $i
+    bash src/tools/experiments/trial.sh data/english_bnews_train.sentences $i
 done
+python src/tools/plotter.py -x "Sentences" -y "Runtime (seconds)" -o "sol/runtimes.png" $RUNTIMES
 
 
 
 
+python src/tools/experiments/analysis.py
+python src/tools/plotter.py -x "Sentences" -y "Precision" -o "sol/precision_recall/precision.png" "sol/precision_recall/precision.txt"
+python src/tools/plotter.py -x "Sentences" -y "Recall" -o "sol/precision_recall/recall.png" "sol/precision_recall/recall.txt"
 
 
-#cat /tmp/LinksToDAG_times
-python src/tools/plotter.py $RUNTIMES
 
-
-
-echo $RUNTIMES $END | mutt -s $END -a $RUNTIMES -- junekihong@gmail.com
+#echo $RUNTIMES $END | mutt -s $END -a $RUNTIMES -- junekihong@gmail.com
