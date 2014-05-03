382b456f46308418c948b019c79d62bbd79a3be5
Juneki Hong <juneki@jhu.edu>
2014-05-03 15:33:15 -0400

    modified trials.sh to use bash variables.

--- trials.sh
+++ trials.sh
@@ -2,25 +2,35 @@
 
 
 RUNTIMES="/tmp/LinksToDAG_times"
-rm -f $RUNTIMES
+SOL_RUNTIME="sol/runtimes.png"
 
+PRECISION_RECALL="sol/precision_recall/"
+TXT_PRECISION=$PRECISION_RECALL"precision.txt"
+TXT_RECALL=$PRECISION_RECALL"recall.txt"
+SOL_PRECISION=$PRECISION_RECALL"precision.png"
+SOL_RECALL=$PRECISION_RECALL"recall.png"
+
+rm -f $RUNTIMES $TXT_RUNTIME $SOL_RUNTIME $TXT_PRECISION $SOL_PRECISION $SOL_RECALL
+rm -rf /tmp/LinksToDAG_trial_sol/
 
 typeset -i i END
-END=100
+END=1000
 
 for ((i=1;i<=$END;i++));
 do
     bash src/tools/experiments/trial.sh data/english_bnews_train.sentences $i
 done
-python src/tools/plotter.py -x "Sentences" -y "Runtime (seconds)" -o "sol/runtimes.png" $RUNTIMES
+
+
+python src/tools/plotter.py -x "Sentences" -y "Runtime (seconds)" -o $SOL_RUNTIME $RUNTIMES
 
 
 
 
 python src/tools/experiments/analysis.py
-python src/tools/plotter.py -x "Sentences" -y "Precision" -o "sol/precision_recall/precision.png" "sol/precision_recall/precision.txt"
-python src/tools/plotter.py -x "Sentences" -y "Recall" -o "sol/precision_recall/recall.png" "sol/precision_recall/recall.txt"
+python src/tools/plotter.py -x "Sentences" -y "Precision" -o $SOL_PRECISION $TXT_PRECISION
+python src/tools/plotter.py -x "Sentences" -y "Recall" -o $SOL_RECALL $TXT_RECALL
 
 
 
-#echo $RUNTIMES $END | mutt -s $END -a $RUNTIMES -- junekihong@gmail.com
+echo $RUNTIMES $END | mutt -s $END -a $RUNTIMES -a $TXT_PRECISION -a $TXT_RECALL  -- junekihong@gmail.com
