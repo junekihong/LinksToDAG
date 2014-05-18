1a44dad1e32118fc9a91d25b2e231cd934509b80
Juneki Hong <junekihong@gmail.com>
2014-05-17 21:59:11 -0400

    updated plotter to handle multiple files. Plotting multiple lines on the same graph.

--- src/tools/plotter.py
+++ src/tools/plotter.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-import math
+import math, argparse, sys
 import matplotlib.pyplot as plt
-import argparse
-import sys
-
+from pprint import pprint
 
 parser = argparse.ArgumentParser(description="Plots a list of x,y points.")
 parser.add_argument("-o", "--output", dest="output", default="sol/runtimes.png",
@@ -18,38 +16,58 @@ parser.add_argument("strings", nargs="*")
 args = parser.parse_args()
 
 
-lines = []
-for f in args.strings:
-    f = open(f)
+labels = []
+file_lines = []
+for filename in args.strings:
+    label = filename.split(".")[0]
+    label = label.split("/")[-1]
+    label = label.split("_")[0]
+    labels.append(label)
+    
+    lines = []
+    f = open(filename)
     f = f.readlines()
     for line in f:
         line = line.strip()
         lines.append(line)
+    file_lines.append(lines)
 
 
 
-Xs = []
-Ys = []
-
-
-for line in lines:
-    if not line:
-        continue
-    if line[0] == "#":
-        continue
-
-
-    line = line.split()
-    x = float(line[0])
-    y = float(line[1])
+file_coord = []
+for lines in file_lines:
+    Xs = []
+    Ys = []
     
-    Xs.append(x)
-    Ys.append(y)
+    coord = []
+
+    for line in lines:
+        if not line:
+            continue
+        if line[0] == "#":
+            continue
+
+        line = line.split()
+        x = float(line[0])
+        y = float(line[1])
+    
+        coord.append((x,y))
+    file_coord.append(coord)
 
 
 # Get the minimum, maximum X values
+xmax = None
 xmin = 0
-xmax = max(Xs)
+for coord in file_coord:
+    temp_max = max(coord)
+    if xmax == None or xmax < temp_max:
+        xmax = temp_max
+
+    temp_min = min(coord)
+    if xmin > temp_min:
+        xmin = temp_min
+xmax = xmax[0]
+
 
 
 plt.figure()
@@ -57,7 +75,15 @@ ax = plt.subplot(111)
 ax.set_xlabel(args.xLabel)
 ax.set_ylabel(args.yLabel)
 
-plt.plot(Xs,Ys)
+
+for l,coord in zip(labels,file_coord):
+    Xs = [elem[0] for elem in coord]
+    Ys = [elem[1] for elem in coord]
+    plt.plot(Xs,Ys, label=l)
+
+
+
+ax.legend(loc='lower right')
 plt.xlim(xmin, xmax)
 plt.ylim(ymin=0)
 
