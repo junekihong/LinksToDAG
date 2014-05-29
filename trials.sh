90ee6786c6e520911d1f23a5586efc4231e2ce6b
Juneki Hong <junekihong@gmail.com>
2014-05-29 19:08:01 -0400

    updated trials.sh to run trial_ru.sh for the russian data.

--- trials.sh
+++ trials.sh
@@ -4,8 +4,8 @@ typeset -i i END
 
 
 #END=10000
-END=60030
-#END=10
+#END=60030
+END=256
 #INCREMENT=1
 
 SENTENCES="data/english_bnews_train.sentences"
@@ -35,22 +35,30 @@ TYPE_AGREEMENT="sol/type_agreement/"
 TYPE_ANALYSIS=$TYPE_AGREEMENT"type_analysis.txt"
 
 
-
-
 rm -f $RUNTIMES $TXT_RUNTIME $SOL_RUNTIME $TXT_PRECISION $SOL_PRECISION $SOL_RECALL
 rm -rf /tmp/LinksToDAG_trial_sol/
+    
 
-CONLL_ANALYSIS="sol/conll_analysis/conll_analysis_"$END".txt"
+# Run the trial_ru.sh script if we are doing russian trials
+if [ $LANGUAGE -a $LANGUAGE = "ru" ]; then
+    for ((i=1;i<=$END;i=$((i*2))));
+    do
+        bash src/tools/experiments/trial_ru.sh $SENTENCES $i
+    done
+    
+    if [ $i -ne $END -a $((i/2)) -ne $END ]; then
+        bash src/tools/experiments/trial_ru.sh $SENTENCES $END
+    fi
+else     
+    for ((i=1;i<=$END;i=$((i*2))));
+    do
+        bash src/tools/experiments/trial.sh $SENTENCES $CONLL $i $LANGUAGE
+    done
 
-for ((i=1;i<=$END;i=$((i*2))));
-do
-    bash src/tools/experiments/trial.sh $SENTENCES $CONLL $i $LANGUAGE
-done
-
-if [ $i -ne $END -a $((i/2)) -ne $END ]; then
-    bash src/tools/experiments/trial.sh $SENTENCES $CONLL $END $LANGUAGE
+    if [ $i -ne $END -a $((i/2)) -ne $END ]; then
+        bash src/tools/experiments/trial.sh $SENTENCES $CONLL $END $LANGUAGE
+    fi
 fi
-
 
 
 
@@ -61,12 +69,23 @@ python src/tools/plotter.py -x "Sentences" -y "Runtime (seconds)" -o $SOL_RUNTIM
 
 
 python src/tools/experiments/type_analysis.py 
-cat $TXT_PRECISION | sort -V > temp; mv temp $TXT_PRECISION
-cat $TXT_RECALL | sort -V > temp; mv temp $TXT_RECALL
+cat $TXT_PRECISION | sort -V > temp_precision; 
+cat $TXT_RECALL | sort -V > temp_recall; 
 
-python src/tools/plotter.py -x "Sentences" -y "Percent" -o $SOL_PRECISION_RECALL $TXT_PRECISION $TXT_RECALL
+if [ $LANGUAGE ]; then
+    mv temp_precision $PRECISION_RECALL"precision#"$LANGUAGE".txt"
+    mv temp_recall $PRECISION_RECALL"recall#"$LANGUAGE".txt"
+    python src/tools/plotter.py -x "Sentences" -y "Percent" -o $PRECISION_RECALL"precision_recall#"$LANGUAGE".png" $PRECISION_RECALL"precision#"$LANGUAGE".txt" $PRECISION_RECALL"recall#"$LANGUAGE".txt"
+else
+    mv temp_precision $TXT_PRECISION
+    mv temp_recall $TXT_RECALL
+    python src/tools/plotter.py -x "Sentences" -y "Percent" -o $SOL_PRECISION_RECALL $TXT_PRECISION $TXT_RECALL
+fi
 
 
 
-echo $RUNTIMES $END $'\n'The CLSP machines cant run plotter. Please download files and run: $'\n'python src/tools/plotter.py -x "Sentences" -y "Percent" -o $SOL_PRECISION_RECALL $TXT_PRECISION $TXT_RECALL | mutt -s $END -a $RUNTIMES -a $TXT_PRECISION -a $TXT_RECALL -a $TYPE_ANALYSIS -a $CONLL_ANALYSIS -- junekihong@gmail.com
+
+
+
+#echo $RUNTIMES $END $'\n'The CLSP machines cant run plotter. Please download files and run: $'\n'python src/tools/plotter.py -x "Sentences" -y "Percent" -o $SOL_PRECISION_RECALL $TXT_PRECISION $TXT_RECALL | mutt -s $END -a $RUNTIMES -a $TXT_PRECISION -a $TXT_RECALL -a $TYPE_ANALYSIS -a $CONLL_ANALYSIS -- junekihong@gmail.com
 
