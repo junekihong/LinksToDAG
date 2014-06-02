6b7c11454f62a9d07918de37ff36e45fd23e79c3
Jason Eisner <jason@cs.jhu.edu>
2014-06-02 16:02:56 -0400

    added sentlen.sh

---
+++ src/sentlen.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Makes a file that maps every sentence ID to that sentence's length.
+cat LinksToDAG_links.txt | perl -ne 'chomp; @f = split(", *"); $len{$f[4]}=$f[0] if $len{$f[4]}<$f[0]; $len{$f[4]}=$f[1] if $len{$f[4]}<$f[1]; END { for $sent (keys %len) { print "$sent, $len{$sent}\n"; } }' > LinksToDAG_sentlen.txt
